f39b78de6719b95349298c9b5d2d665b2d18b34b
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2023-04-10 11:30:29 -0500

    feat: :sparkles: Consultas
    Se añadio un apartado de consultas

--- components/my-component.js
+++ components/my-component.js
@@ -1,49 +1,96 @@
-export default{
-    data(){
-        return{
-            sedes:['Medellin',
-            'Bogota',
-            'Bucaramanga',
-            'Mexico'],
-            selecCiudad:'',
-            roles:['Estudiante'
-            ,'Profesor'],
-            selecRol:'',
-            rol:null,
-            tecnoglogias:['ADSO','Electricidad','Mecanica'],
-            campus:{
+export default {
+    data() {
+        return {
+            sedes: ['Medellin',
+                'Bogota',
+                'Bucaramanga',
+                'Mexico'],
+
+            roles: ['Estudiante'
+                , 'Profesor'],
+            selecRol: '',
+            rol: null,
+            tecnologiaPer: '',
+            tecnologias: ['ADSO'
+                , 'Electricidad'
+                , 'Mecanica'],
+            selecFormacion: '',
+            eleccionFormacion: ['ELECTIVA',
+                'OBLIGATORIA'
+            ],
+            tipoFormacion:['Remota',
+            'Presencial'],
+            mapeo:[],
+            consulta:{},
+            bool:false,
+
+            campus: {
                 Sedes:
-                {ciudCampus:'',
-                telefono:'',
-                direccion:''
-            },
-                estudiante:{
-                    nombre:'',
-                    apellido:'',
-                    telefono:'',
-                    fecha:'',
-                    hora:'',
-                    piso:'',
-                    salon:'',
-                    email:''
+                {
+                    ciudCampus: '',
+                    telefono: '',
+                    direccion: ''
                 },
-                profesor:{
-                    nombre:'',
-                    apellido:'',
-                    telefono:'',
-                    fecha:'',
-                    hora:'',
-                    piso:'',
-                    salon:'',
-                    email:''
+                estudiante: {
+                    nombreEstu: '',
+                    apellidoEstu: '',
+                    telefonoEstu: '',
+                    fechaEstu: '',
+                    horaEstu: '',
+                    pisoEstu: '',
+                    salonEstu: '',
+                    emailEstu: '',
+                    transporteEstu: '',
+                    barrioEstu: ''
                 },
-            }
+                profesor: {
+                    nombreProf: '',
+                    apellidoProf: '',
+                    telefonoProf: '',
+                    fechaProf: '',
+                    horaProf: '',
+                    pisoProf: '',
+                    salonProf: '',
+                    emailProf: ''
+                },
+                tecnologia: {
+                        nombreADSO: 'ADSO',
+                        preRequisitoADSO: 'SI',
+                        requisitoADSO:'Titulo: Tecnico en sistemas',
+                        eleccionFormacionADSO: '',
+                        pruebaPreSelecADSO: 'NO',
+                        numCreditosADSO: '',
+                        numAsignaturaADSO: '',
+                        tipoFormacionADSO:'',
+                    
+                        nombreElec: 'Electricidad',
+                        preRequisitoElec: 'NO',
+                        eleccionFormacionElec: '',
+                        pruebaPreSelecElec: 'NO',
+                        numCreditosElec: '',
+                        numAsignaturaElec: '',
+                        tipoFormacionElec:'',
+                    
+                        nombreMec: 'Mecanica',
+                        preRequisitoMec: 'NO',
+                        eleccionFormacionMec: '',
+                        pruebaPreSelecMec: 'NO',
+                        numCreditosMec: '',
+                        numAsignaturaMec: '',
+                        tipoFormacionMec:'',
+                }
         }
+    }
+},
+methods: {
+    buttonEnviar() {
+        this.mapeo = {...this.campus}
+        
     },
-    methods:{
-        buttonEnviar() {
-            console.log(this.campus)
-        }
-    },
-     
+    consultaDestructuring(){
+        this.bool = !this.bool
+    }
+
+}
+
 }
